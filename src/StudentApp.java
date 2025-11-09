f0747a38a37fc997e50ec79b72d01e538870757e
EnderAres <love2fish183@gmai.com>
2025-11-09 12:11:30 -0500

    more comments

--- src/StudentApp.java
+++ src/StudentApp.java
@@ -1,21 +1,19 @@
-import java.sql.*;
+import java.sql.*; //imports classes to connect to database
 import java.sql.Date;
 import java.util.Scanner;
 
 
 
 public class StudentApp {
-
+    //database connection information
     private static final String URL = "jdbc:postgresql://localhost:5432/school_db"; // database URL
     private static final String USER = "postgres"; // database username
     private static final String PASSWORD = "COMP3005"; //database password
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //scanner for user input
         int choice;
-
-
 
         do { //start menu
             System.out.println("\n--- Student database menu ---");
@@ -29,9 +27,9 @@ public class StudentApp {
 
             switch (choice) {
                 case 1:
-                    getAllStudents();
+                    getAllStudents(); //display all students from database
                     break;
-                case 2:
+                case 2: //prompt user for student details then adds the student
                     System.out.print("First name: ");
                     String firstName = scanner.next();
                     System.out.print("Last name: ");
@@ -42,19 +40,19 @@ public class StudentApp {
                     String date = scanner.next();
                     addStudent(firstName, lastName, email, date);
                     break;
-                case 3:
+                case 3: //updates email
                     System.out.print("Student ID: ");
                     int id = scanner.nextInt();
                     System.out.print("New email: ");
                     String newEmail = scanner.next();
                     updateStudentEmail(id, newEmail);
                     break;
-                case 4:
+                case 4: //delate a student record by using ID
                     System.out.print("Student ID to delete: ");
                     int delId = scanner.nextInt();
                     deleteStudent(delId);
                     break;
-                case 5:
+                case 5: //exit
                     System.out.println("Exiting program...");
                     break;
                 default:
@@ -64,7 +62,7 @@ public class StudentApp {
             }
 
         }
-        while (choice != 5);
+        while (choice != 5); //continues loop until exit
         scanner .close();
 
     }
